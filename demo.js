090c5c70e106f5d731f4396b0236c45a9d8c7830
Tyler Florez <qualiabyte@gmail.com>
2014-04-27 10:09:51 -0700

    Add EBNF grammar for objects

--- demo.js
+++ demo.js
@@ -3,14 +3,18 @@ var diagram = require('ebnf-diagram');
 var exec = require('child_process').exec;
 
 var grammars = {
-  simple: fs.readFileSync('./grammars/simple.grammar', 'utf8')
-, literal: '"My literal grammar" { a = "a". } "Pretty neat!"'
+  literal: 'a = "a".'
+, simple: fs.readFileSync('./grammars/simple.grammar', 'utf8')
+, objects: fs.readFileSync('./grammars/objects.ebnf', 'utf8')
 };
 
 var renderGrammars = function() {
   for (var type in grammars) {
     var grammar = grammars[type];
-    diagram.fromText(grammar, './build/' + type + '.png', 800, 600);
+    var title = 'My ' + type + ' grammar.';
+    var comment = "Pretty neat!";
+    var input = '"' + title + '" {\n' + grammar + '\n}"' + comment + '"';
+    diagram.fromText(input, './build/' + type + '.png', 2000, 4000);
   }
 };
 
